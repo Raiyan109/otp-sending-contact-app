d9002ea0aee24232e1a5ba765b750ea9f2cc4ea8
Raiyan109 <kraiyan109@gmail.com>
2023-01-28 21:57:52 +0600

    stucked

--- src/components/SendMessage/SendMessage.js
+++ src/components/SendMessage/SendMessage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './SendMessage.module.css'
 const SendMessage = () => {
     return (
-        <div>
-            <label>
-                <input type="text" />
-            </label>
-            <button>Send</button>
+        <div className={styles.container}>
+            <form>
+                <h1>Send OTP </h1>
+                <input type="text" placeholder='Enter phone number e.g. 1555334345343' />
+                <br />
+                <button type='sumbit' name='button'>Send</button>
+            </form>
         </div>
     );
 };
